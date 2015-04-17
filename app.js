286d7ebfd7b2ff960f894f1acf9a6867d4a54885
root <root@115elmp02.blackmesh.com>
2015-04-17 13:06:32 -0400

    added basic auth

--- app.js
+++ app.js
@@ -9,6 +9,38 @@ var routes = require('./routes/index');
 
 var app = express();
 
+// Authenticator
+app.use(function(req, res, next) {
+    var auth;
+
+    // check whether an autorization header was send    
+    if (req.headers.authorization) {
+      // only accepting basic auth, so:
+      // * cut the starting "Basic " from the header
+      // * decode the base64 encoded username:password
+      // * split the string at the colon
+      // -> should result in an array
+      auth = new Buffer(req.headers.authorization.substring(6), 'base64').toString().split(':');
+    }
+
+    // checks if:
+    // * auth array exists 
+    // * first value matches the expected user 
+    // * second value the expected password
+    if (!auth || auth[0] !== 'testuser' || auth[1] !== 'testpassword') {
+        // any of the tests failed
+        // send an Basic Auth request (HTTP Code: 401 Unauthorized)
+        res.statusCode = 401;
+        // MyRealmName can be changed to anything, will be prompted to the user
+        res.setHeader('WWW-Authenticate', 'Basic realm="MyRealmName"');
+        // this will displayed in the browser when authorization is cancelled
+        res.end('Unauthorized');
+    } else {
+        // continue with processing, user was authenticated
+        next();
+    }
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
